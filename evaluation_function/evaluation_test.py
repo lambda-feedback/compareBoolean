a169bb56c064e53e1669c94e41d7aca97b4c2ff7
Peter Johnson <peterbjohnson@Peters-MacBook-Pro-2.local>
2024-07-19 16:17:31 +0100

    demo

--- evaluation_function/evaluation_test.py
+++ evaluation_function/evaluation_test.py
@@ -1,6 +1,8 @@
 import unittest
+import json
 
 from .evaluation import Params, evaluation_function
+
 
 class TestEvaluationFunction(unittest.TestCase):
     """
@@ -20,6 +22,19 @@ class TestEvaluationFunction(unittest.TestCase):
     Use evaluation_function() to check your algorithm works
     as it should.
     """
+    # Utility to help extract test info for documentation:
+
+    def docs_from_test(self, context):
+        docs = {
+            'response': context['response'],
+            'answer': context['answer'],
+            'params': context['params'],
+            'is_correct': context['result'].get("is_correct"),
+            'feedback': context['result'].get("feedback")
+        }
+        print(json.dumps(docs))
+
+    # Tests
 
     def test_returns_is_correct_true_ascii(self):
         response, answer, params = "A & Test", "A & Test", Params()
@@ -27,9 +42,11 @@ class TestEvaluationFunction(unittest.TestCase):
         result = evaluation_function(response, answer, params).to_dict()
 
         self.assertEqual(result.get("is_correct"), True)
-        self.assertEqual(result.get("response_latex"), "A \\cdot \mathrm{Test}")
+        self.assertEqual(result.get("response_latex"),
+                         "A \\cdot \mathrm{Test}")
         self.assertFalse(result.get("feedback"))
-    
+        self.docs_from_test(locals())
+
     def test_returns_negative(self):
         response, answer, params = "A | B", "A & B", Params()
 
@@ -38,7 +55,8 @@ class TestEvaluationFunction(unittest.TestCase):
         self.assertEqual(result.get("is_correct"), False)
         self.assertEqual(result.get("response_latex"), "A + B")
         self.assertTrue(result.get("feedback"))
-    
+        self.docs_from_test(locals())
+
     def test_syntax_error(self):
         response, answer, params = "A £ B", "A & B", Params()
 
@@ -47,7 +65,7 @@ class TestEvaluationFunction(unittest.TestCase):
             self.fail("Exception not raised for unexpected token")
         except:
             pass
-    
+
     def test_xor_identity(self):
         response, answer, params = "A ^ B", "A & ~B | ~A & B", Params()
 
@@ -56,7 +74,7 @@ class TestEvaluationFunction(unittest.TestCase):
         self.assertEqual(result.get("is_correct"), True)
         self.assertEqual(result.get("response_latex"), "A \\oplus B")
         self.assertFalse(result.get("feedback"))
-    
+
     def test_nand_or(self):
         response, answer, params = "A | B", "~(~A & ~B)", Params()
 
@@ -65,7 +83,7 @@ class TestEvaluationFunction(unittest.TestCase):
         self.assertEqual(result.get("is_correct"), True)
         self.assertEqual(result.get("response_latex"), "A + B")
         self.assertFalse(result.get("feedback"))
-    
+
     def test_nand_or(self):
         response, answer, params = "A | B", "~(~A & ~B)", Params()
 
@@ -81,11 +99,13 @@ class TestEvaluationFunction(unittest.TestCase):
         result = evaluation_function(response, answer, params).to_dict()
 
         self.assertEqual(result.get("is_correct"), True)
-        self.assertEqual(result.get("response_latex"), "\\overline{\\left( A \\cdot B \\right)}")
+        self.assertEqual(result.get("response_latex"),
+                         "\\overline{\\left( A \\cdot B \\right)}")
         self.assertFalse(result.get("feedback"))
-    
+
     def test_complex(self):
-        response, answer, params = "A & B | B & C & (B | C)", "B & (A | C)", Params()
+        response, answer, params = "A & B | B & C & (B | C)", "B & (A | C)", Params(
+        )
 
         result = evaluation_function(response, answer, params).to_dict()
 
